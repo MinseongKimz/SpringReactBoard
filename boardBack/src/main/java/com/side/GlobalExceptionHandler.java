11cc1afdeacf5185a8652db2e3beea58ea219303
kms <minseongkimc@gmail.com>
2023-12-09 20:33:30 +0900

    커스텀 에러 만들어서 api 리턴 시키기

--- boardBack/src/main/java/com/side/GlobalExceptionHandler.java
+++ boardBack/src/main/java/com/side/GlobalExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 @Slf4j
@@ -32,6 +34,28 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(ErrorCode.METHOD_NOT_ALLOWED.getStatus().value())
                 .body(new ErrorResponse(ErrorCode.METHOD_NOT_ALLOWED));
+    }
+    /*
+    * PathVariable null 일시
+    * 400 return
+    * */
+    @ExceptionHandler(NoResourceFoundException.class)
+    protected ResponseEntity<ErrorResponse> handleNoResFoundException(final NoResourceFoundException e) {
+        log.warn("NoResourceFoundException", e);
+        return  ResponseEntity
+                .status(ErrorCode.BAD_REQUEST.getStatus().value())
+                .body(new ErrorResponse(ErrorCode.BAD_REQUEST));
+    }
+    /*
+     * PathVariable 타입 불일치 시
+     * 400 return
+     * */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentTypeMismatchException(final MethodArgumentTypeMismatchException e) {
+        log.warn("MethodArgumentTypeMismatchException", e);
+        return ResponseEntity
+                .status(ErrorCode.BAD_REQUEST.getStatus().value())
+                .body(new ErrorResponse(ErrorCode.BAD_REQUEST));
     }
 
     /*
